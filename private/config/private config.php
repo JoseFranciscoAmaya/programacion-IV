0b3decd3ffaead899a337a13dedca84d8a65aa88
Ernesto1020 <marthynezjose100217@gmail.com>
2020-03-20 21:27:40 +0100

    app

---
+++ private/config/private config.php
@@ -0,0 +1,8 @@
+/**
+ * Importamos la clase principal de conexion
+ */
+include('../Conexion/DB.php');
+$conexion = DB('localhost','root','','db_app_academica');
+include('../../Conexion/DB.php');
+$conexion = new DB('localhost','root','','db_app_academica');
+?> 
